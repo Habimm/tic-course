5edc5ccd647005695919da301aff564920c863ec
Habimm <habimm@pm.me>
2022-12-13 17:02:27 +0700

    Step 6: Constructing the ReactJS state

--- game.jsx
+++ game.jsx
@@ -1,26 +1,56 @@
+class Board extends React.Component {
+
+  renderSquare(i) {
+    var text = this.props.board[i]
+    if (text === '-') {
+      text = null
+    }
+    return (
+      <button className='square'>
+        {text}
+      </button>
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+
+        <div>
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+
+        <div>
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    )
+  }
+}
+
 class Game extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      board: ['-', '-', '-', '-', '-', '-', '-', '-', '-'],
+      isXTurn: true,
+    }
+  }
+
   render() {
     return (
       <div className='game'>
-        <div>
-          <div>
-            <button className='square'>X</button>
-            <button className='square'>X</button>
-            <button className='square'>X</button>
-          </div>
-
-          <div>
-            <button className='square'>X</button>
-            <button className='square'>X</button>
-            <button className='square'>X</button>
-          </div>
-
-          <div>
-            <button className='square'>X</button>
-            <button className='square'>X</button>
-            <button className='square'>X</button>
-          </div>
-        </div>
+        <Board board={this.state.board}/>
         <div className='game-info'>
           <div>Next player: O</div>
           <ol><button>Reset game</button></ol>
